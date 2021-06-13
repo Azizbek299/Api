565fec43704d42d0e02b349ffbc324e916136584
Azizbek299 <61055356+Azizbek299@users.noreply.github.com>
2021-06-13 17:56:10 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ sleep 10
 
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
 gunicorn greatesoft.wsgi:application --bind 0.0.0.0:8000 --reload
 
 exec "$@"
